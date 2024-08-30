ed100f7f923275b80034a4846f1c4eee2be71eff
yan <y-su@unext.jp>
2024-08-30 21:52:44 +0900

    .

--- foo/package.go
+++ foo/package.go
@@ -3,6 +3,7 @@ package foo
 import (
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
+    "github.com/redis/go-redis/v9"
 )
 
 func Foo() *prometheus.Registry {
@@ -13,4 +14,8 @@ func Bar(reg *prometheus.Registry) prometheus.GaugeFunc {
 	return promauto.With(reg).NewGaugeFunc(prometheus.GaugeOpts{}, func() float64 {
 		return 0
 	})
+}
+
+func Baz() *redis.Client {
+	return redis.NewClient(&redis.Options{})
 }
